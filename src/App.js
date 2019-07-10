33a0a42461c701a117c9bcd9c25abb89a5fa8b26
Madamot <adam@madamot.com>
2019-07-11 00:29:25 +0100

    added sample colours list and simple adding chosen colour to state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const colourList = [
+  {
+    name: 'Orange Princess',
+    hex: '#FFA500',
+    price: '5',
+    objectID: 0,
+  },
+  {
+    name: 'Michelle',
+    hex: '#b0e0e6',
+    price: '20',
+    objectID: 1,
+  },
+  {
+    name: 'grey',
+    hex: '#f6f6f6',
+    price: '25',
+    objectID: 2,
+  },
+];
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colourList: colourList,
+      chosenColour: "",
+    }
+
+    this.onColourPick = this.onColourPick.bind(this);
+  }
+
+  onColourPick(colour) {
+    this.setState({ chosenColour: colour }, () => console.log(this.state.chosenColour));
+
+  }
+
   render() {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {colourList.map(colour =>
+          <button key={colour.objectID} onClick={() => this.onColourPick(colour)} type="button" className="colBox">
+            <span>
+              <div className="colourPeak" style={{ backgroundColor: colour.hex }}></div>
+            </span>
+            <span>
+              <br/>{colour.name}
+            </span>
+          </button>
+        )}
       </div>
     );
   }
 }
+
 
 export default App;
