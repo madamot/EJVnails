51ef670d87517be2ca8f91ecc0f7487321f820f6
Madamot <adam@madamot.com>
2019-07-11 11:45:05 +0100

    toggle functionality and improved styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React, { Component } from 'react';
 import './App.css';
+import Toggle from './Toggle';
 
 const colourList = [
   {
-    name: 'Orange Princess',
+    name: 'BIAB',
     hex: '#FFA500',
     price: '5',
     objectID: 0,
   },
   {
-    name: 'Michelle',
+    name: 'Hard Gel',
     hex: '#b0e0e6',
     price: '20',
     objectID: 1,
   },
   {
-    name: 'grey',
+    name: 'Acrylic',
     hex: '#f6f6f6',
     price: '25',
     objectID: 2,
+  },
+  {
+    name: 'BIAB',
+    hex: '#FFA500',
+    price: '5',
+    objectID: 3,
+  },
+  {
+    name: 'Hard Gel',
+    hex: '#b0e0e6',
+    price: '20',
+    objectID: 4,
+  },
+  {
+    name: 'Acrylic',
+    hex: '#f6f6f6',
+    price: '25',
+    objectID: 5,
+  },
+  {
+    name: 'Acrylic',
+    hex: '#f6f6f6',
+    price: '25',
+    objectID: 6,
+  },
+  {
+    name: 'BIAB',
+    hex: '#FFA500',
+    price: '5',
+    objectID: 7,
+  },
+  {
+    name: 'Hard Gel',
+    hex: '#b0e0e6',
+    price: '20',
+    objectID: 8,
+  },
+  {
+    name: 'Acrylic',
+    hex: '#f6f6f6',
+    price: '25',
+    objectID: 9,
   },
 ];
 
@@ -41,11 +84,70 @@ class App extends Component {
   }
 
   render() {
+    const { colourList, chosenColour } = this.state;
 
     return (
       <div className="App">
+        <div className="interactions">
+          <div className="sideBarContainer">
+            <Toggle>
+              {({ on, toggle }) => (
+                <div className="sideBar">
+                  {on && <Table
+                    colourList={colourList}
+                    onColourPick={this.onColourPick}
+                         />}
+                  <button onClick={toggle}>Base</button>
+                </div>
+              )}
+            </Toggle>
+            <Toggle>
+              {({ on, toggle }) => (
+                <div className="sideBar">
+                  {on && <h2>hi</h2>}
+                  <button onClick={toggle}>Colours</button>
+                </div>
+              )}
+            </Toggle>
+            <Toggle>
+              {({ on, toggle }) => (
+                <div className="sideBar">
+                  {on && <h2>hi</h2>}
+                  <button onClick={toggle}>Shape</button>
+                </div>
+              )}
+            </Toggle>
+            <Toggle>
+              {({ on, toggle }) => (
+                <div className="sideBar">
+                  {on && <h2>hi</h2>}
+                  <button onClick={toggle}>Length</button>
+                </div>
+              )}
+            </Toggle>
+          </div>
+
+
+          <Cost price={chosenColour.price}>
+            { chosenColour
+              ? <h3>Total</h3>
+              : null
+            }
+          </Cost>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { onColourPick } = this.props;
+    return (
+      <div>
         {colourList.map(colour =>
-          <button key={colour.objectID} onClick={() => this.onColourPick(colour)} type="button" className="colBox">
+          <button key={colour.objectID} onClick={() => onColourPick(colour)} type="button" className="colBox">
             <span>
               <div className="colourPeak" style={{ backgroundColor: colour.hex }}></div>
             </span>
@@ -58,6 +160,18 @@ class App extends Component {
     );
   }
 }
+
+class Cost extends Component {
+  render() {
+    const { price, children } = this.props;
+    return (
+      <div className="bottomBar">
+        {children}£{price}
+      </div>
+    );
+  }
+}
+
 
 
 export default App;
